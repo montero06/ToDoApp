fe7ed9ce933a93f83224e87a0f0f9d535085db2d
juanmonterogalandam <juanmonterogalandam@gmail.com>
2026-02-11 20:44:08 +0100

    Ficheros

--- app/src/main/java/com/example/tarealarga1trimestre/Activity/CrearEditar/Fragmento2.java
+++ app/src/main/java/com/example/tarealarga1trimestre/Activity/CrearEditar/Fragmento2.java
@@ -13,6 +13,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
